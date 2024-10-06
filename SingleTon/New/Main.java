db5858ad2e9dcd82eddc5ef067b1ee0fd4424b78
kamrul <bsse1418@iit.du.ac.bd>
2024-10-06 23:45:48 +0600

    myf

---
+++ SingleTon/New/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+          public static void main(String args[]){
+                    Single single=Single.initiate();
+                    single.dataUpdate();
+                    Single single2=Single.initiate();
+                    single2.dataUpdate();
+
+          }
+}
