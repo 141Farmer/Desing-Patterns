d8768eb4c2e961ca199ca73bfe1ad45b2229e4e7
kamrul <bsse1418@iit.du.ac.bd>
2025-01-26 23:14:58 +0600

    almost

--- Creational/SingleTon/Class/AbstractFactory.java
+++
@@ -1,6 +0,0 @@
-public abstract class AbstractFactory{
-
-    public abstract FurnitureModern createProductModern();
-    public abstract FurnitureRustic createProductRustic();
-    public abstract FurnitureVictorian createProductVictorian();
-}
--- Creational/SingleTon/New/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main{
-          public static void main(String args[]){
-                    Single single=Single.initiate();
-                    single.dataUpdate();
-                    Single single2=Single.initiate();
-                    single2.dataUpdate();
-
-          }
-}
