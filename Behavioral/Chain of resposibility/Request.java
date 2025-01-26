1ac03a85e48681c7dd4ce8a25221762d22fe99ed
kamrul <bsse1418@iit.du.ac.bd>
2025-01-27 00:40:11 +0600

    chain

---
+++ Behavioral/Chain of resposibility/Request.java
@@ -0,0 +1,12 @@
+class Request {
+          private Priority priority;
+      
+          public Request(Priority priority) {
+              this.priority = priority;
+          }
+      
+          public Priority getPriority() {
+              return priority;
+          }
+      }
+      
